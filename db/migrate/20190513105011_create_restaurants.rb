6d7a5dd0775481e2e8b55d1a493892cdbdd56f9c
Yanti Suryati <yantisc@gmail.com>
2019-05-13 11:52:06 +0100

    Add restaurant model

--- db/migrate/20190513105011_create_restaurants.rb
+++ db/migrate/20190513105011_create_restaurants.rb
@@ -1,6 +1,11 @@
 class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.integer :phone_number
+      t.string :category
+      t.integer :review_id
 
       t.timestamps
     end
